e0d093e177f14f044e7ed561ac61e4eddfc3e7f1
Dinesh R <rdineshrdp@gmail.com>
2025-03-26 09:35:29 +0530

    Favicon updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,8 @@ export const metadata: Metadata = {
   description:
     "Elevate your brand with Manjiro Tech. We provide innovative digital & IT solutions designed to drive results.",
   icons: {
-    icon: '/favicon.ico',
-    apple: '/favicon.ico',
+    icon: 'manjirotech/favicon.ico',
+    apple: 'manjirotech/favicon.ico',
   },
 }
 
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="manjirotech/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
